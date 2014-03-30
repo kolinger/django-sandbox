6cc0ca153e252037533b82ca10881581cd4c33d1
Tomáš Kolinger <tomas@kolinger.name>
2014-03-30 20:57:05 +0200

    updated code style

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url, include
 from django.contrib import admin
 
 urlpatterns = patterns('',
-   url(r'^admin/', include(admin.site.urls)),
-   url(r'^$', 'pages.views.default'),
-   url(r'^detail/(\w+)$', 'pages.views.detail'),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', 'pages.views.default'),
+                       url(r'^detail/(\w+)$', 'pages.views.detail'),
 )
