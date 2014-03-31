f60b8e42deaeb8d8f94b40508ced8c823ff3bfef
Tomáš Kolinger <tomas@kolinger.name>
2014-03-31 17:01:44 +0200

    some improvements

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 from pages.models import Page
 
@@ -6,10 +7,10 @@ from pages.models import Page
 def default(request):
     return render_to_response("pages.html", {
         "pages": Page.objects.all()
-    })
+    }, context_instance=RequestContext(request))
 
 
 def detail(request, slug):
     return render_to_response("page.html", {
         "page": Page.objects.get(slug=slug)
-    })
+    }, context_instance=RequestContext(request))
