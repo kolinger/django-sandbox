ed61037878af0309cafb223be813b58adcf13468
Tomáš Kolinger <tomas@kolinger.name>
2014-03-31 12:43:03 +0200

    improved slug field

--- pages/models.py
+++ pages/models.py
@@ -1,16 +1,32 @@
+from django import forms
 from django.db import models
 from django.contrib import admin
+from django.template.defaultfilters import slugify
 
 
 class Page(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    slug = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
     content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Page, self).save(*args, **kwargs)
+
+
+class PageForm(forms.ModelForm):
+    def clean_name(self):
+        data = self.cleaned_data["name"]
+        if Page.objects.filter(slug=slugify(data)).exists():
+            raise forms.ValidationError("This name is already used")
+        return data
 
 
 class PageAdmin(admin.ModelAdmin):
+    fields = ("name", "content")
     list_display = ("name", "slug")
+    form = PageForm
 
 
 admin.site.register(Page, PageAdmin)
